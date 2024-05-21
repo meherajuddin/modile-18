095b36b3b1b63a8b5a55170f733b0a2e71551b42
meherajuddin <meherajuddinantor@gmail.com>
2024-05-21 12:35:38 +0600

    god

---
+++ fectis.js
@@ -0,0 +1,9 @@
+function sun(num1, num2) {
+  const getTotal = num1 + num2;
+  const total = getTotal / 2;
+  return total;
+}
+const num11 = 25;
+const num22 = 60;
+const sun2 = sun(num11, num22);
+console.log("dan", sun2);
