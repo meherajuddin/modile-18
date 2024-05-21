9ef1f6df2d1a8b9ab604dab13fddd8d2a65747b3
meherajuddin <meherajuddinantor@gmail.com>
2024-05-21 17:28:42 +0600

    switch

--- fectis.js
+++ fectis.js
@@ -1,17 +1,31 @@
-// function sun(num1, num2) {
-//   const getTotal = num1 + num2;
-//   const total = getTotal / 2;
-//   return total;
-// }
-// const num11 = 25;
-// const num22 = 60;
-// const sun2 = sun(num11, num22);
-// console.log("dan", sun2);
-const color = "";
+function sun(num1, num2) {
+  const getTotal = num1 + num2;
+  const total = getTotal / 2;
+  return total;
+}
+const num11 = 25;
+const num22 = 60;
+const sun2 = sun(num11, num22);
+console.log("dan", sun2);
+
+const color = "red";
 if (color === "green") {
-  console.log("you can go");
+  console.log("signal: You can cross the road");
 } else if (color === "red") {
-  console.log("you can't go");
+  console.log("signal: Can't cross the road");
 } else {
-  console.log("wait");
+  console.log("signal: wait");
+}
+const colors = "green";
+switch (colors) {
+  case "green":
+    console.log("signal: You can cross the road");
+    break;
+  case "red":
+    console.log("signal: Can't cross the road");
+    break;
+
+  default:
+    console.log("signal: wait");
+    break;
 }
