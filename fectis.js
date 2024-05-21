4dec26c13cd1bf83e41ad9903dd8424b1843477b
meherajuddin <meherajuddinantor@gmail.com>
2024-05-21 17:10:13 +0600

    tapik

--- fectis.js
+++ fectis.js
@@ -1,9 +1,17 @@
-function sun(num1, num2) {
-  const getTotal = num1 + num2;
-  const total = getTotal / 2;
-  return total;
+// function sun(num1, num2) {
+//   const getTotal = num1 + num2;
+//   const total = getTotal / 2;
+//   return total;
+// }
+// const num11 = 25;
+// const num22 = 60;
+// const sun2 = sun(num11, num22);
+// console.log("dan", sun2);
+const color = "";
+if (color === "green") {
+  console.log("you can go");
+} else if (color === "red") {
+  console.log("you can't go");
+} else {
+  console.log("wait");
 }
-const num11 = 25;
-const num22 = 60;
-const sun2 = sun(num11, num22);
-console.log("dan", sun2);
